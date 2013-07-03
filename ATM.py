e088b19351f6df92ea414125a415f78f9b9de382
Glen-Andrew <glen-andrew@hotmail.com>
2013-07-03 17:28:54 +1000

    Working Version

--- ATM.py
+++ ATM.py
@@ -3,3 +3,15 @@ def ATM(amount):
 	# ATM machine change given in $50s and $20s
 	initalAmount = amount
 	n50s = 0;
+	n20s = 0;
+	finish = False;
+	
+	while finish == False:
+		if amount % 50 == 0:
+			n50s = amount / 50;
+			finish = True;
+		else:
+			amount = amount - 20;
+			n20s = n20s + 1;
+	print ("you have withdrawn " + str(amount+(n20s*20)));
+	print ("you will receive " + str(n50s) + " $50 notes and " + str(n20s) + " $20 notes");
